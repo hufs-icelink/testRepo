a0636a8690627e4676e1517e36bcf2faaa600bc3
erica0321 <hyunahrho29@gmail.com>
2023-03-28 03:06:11 +0900

    SignUp page

--- icelink-frontend/frontend-react/src/SignUp.js
+++ icelink-frontend/frontend-react/src/SignUp.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./style/SignUp.css"
 
 function SignUp(props) {
-  return <h2>여기는 회원가입페이지입니다..</h2>;
+  return (
+    	<div class="main-container">
+        <div class="main-wrap">
+          <section class="signUp-input-section-wrap">
+            <div class="signUp-input-wrap">
+              <input type="text" name="userName" id="userName" placeholder="닉네임" />
+            </div>
+            <div class="signUp-input-wrap padding-wrap">
+              <input type="text" name="userId" id="userId" placeholder="아이디" />
+            </div>
+            <div class="signUp-input-wrap padding-wrap">
+                <input type="password" name="userPass" id="userPass" placeholder="비밀번호" />
+            </div>
+            <div class="signUp-input-wrap padding-wrap">
+              <input type="email" name="userEmail" id="userEmail" placeholder="이메일" />
+            </div>
+            <div class="signUp-input-wrap padding-wrap">
+              <input type="text" name="userGithub" id="userGithub" placeholder="Github 주소" />
+            </div>			
+          </section>
+         </div>
+        <div class="signUp-button-wrap">
+            <button>Sign Up</button>
+        </div>
+      </div>
+  );
 }
 
 export default SignUp;
