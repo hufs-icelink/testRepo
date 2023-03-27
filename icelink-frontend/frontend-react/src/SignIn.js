9f4c5fcaef06d27362f95bc6c73198c83cc9aa24
erica0321 <hyunahrho29@gmail.com>
2023-03-28 03:06:04 +0900

    SignIn page

--- icelink-frontend/frontend-react/src/SignIn.js
+++ icelink-frontend/frontend-react/src/SignIn.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./style/SignIn.css"
 
 function SignIn(props) {
-  return <h2>여기는 로그인페이지입니다.</h2>;
+  return (
+  <div class="main-container">
+    <div class="main-wrap">
+      <section class="login-input-section-wrap">
+        <div class="login-input-wrap">	
+          <input placeholder="Username" type="text"></input>
+        </div>
+        <div class="login-input-wrap password-wrap">	
+          <input placeholder="Password" type="password"></input>
+        </div>
+        <div class="login-button-wrap">
+          <button>Sign in</button>
+        </div>
+      </section>
+  </div>
+</div>
+  );
 }
 
 export default SignIn;
