b7e022079d0dafadeb6bf8a88674511fc35d5aa4
erica0321 <hyunahrho29@gmail.com>
2023-03-28 17:16:58 +0900

    Update SignUp.js

--- icelink-frontend/frontend-react/src/SignUp.js
+++ icelink-frontend/frontend-react/src/SignUp.js
@@ -7,24 +7,24 @@ function SignUp(props) {
         <div class="main-wrap">
           <section class="signUp-input-section-wrap">
             <div class="signUp-input-wrap">
-              <input type="text" name="userName" id="userName" placeholder="닉네임" />
+              <input type="text" name="userName" id="userName" placeholder="Name" />
             </div>
             <div class="signUp-input-wrap padding-wrap">
-              <input type="text" name="userId" id="userId" placeholder="아이디" />
+              <input type="text" name="userId" id="userId" placeholder="User ID" />
             </div>
             <div class="signUp-input-wrap padding-wrap">
-                <input type="password" name="userPass" id="userPass" placeholder="비밀번호" />
+                <input type="password" name="userPass" id="userPass" placeholder="Password" />
             </div>
             <div class="signUp-input-wrap padding-wrap">
-              <input type="email" name="userEmail" id="userEmail" placeholder="이메일" />
+              <input type="email" name="userEmail" id="userEmail" placeholder="Email" />
             </div>
             <div class="signUp-input-wrap padding-wrap">
-              <input type="text" name="userGithub" id="userGithub" placeholder="Github 주소" />
+              <input type="text" name="userGithub" id="userGithub" placeholder="Github address" />
             </div>			
           </section>
-         </div>
         <div class="signUp-button-wrap">
             <button>Sign Up</button>
+        </div>
         </div>
       </div>
   );
