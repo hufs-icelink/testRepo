b6f59e4be4da92f71c4ea2642d43395c2b6a7be2
yunbeen <kanado20000@naver.com>
2023-03-28 15:04:27 +0900

    .

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 }
 
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -2,13 +2,24 @@ package com.example.demo.service;
 
 import com.example.demo.dto.MemberDTO;
 import com.example.demo.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
+@Configuration
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberRepository memberRepository;
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemberRepository(em);
+    }
 
     public void save(MemberDTO memberDTO) {
     }
