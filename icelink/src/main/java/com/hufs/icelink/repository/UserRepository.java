732df8657598dc3a5883b8661d041b5195560690
kanado5385-k <81556800+kanado5385-k@users.noreply.github.com>
2023-04-11 12:32:02 +0900

    Delete icelink directory

--- icelink/src/main/java/com/hufs/icelink/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hufs.icelink.repository;
-
-import com.hufs.icelink.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
