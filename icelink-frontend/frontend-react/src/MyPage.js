0f65bc48d58d33b0f7473df8a9e29630d7a26023
Hyun ah Rho <81230764+erica0321@users.noreply.github.com>
2023-05-07 01:49:06 +0900

    수정

--- icelink-frontend/frontend-react/src/MyPage.js
+++ icelink-frontend/frontend-react/src/MyPage.js
@@ -1,114 +1,78 @@
 import React from "react";
+import myImg from "./img/jjanggu.jpg";
+import "./style/MyPage.css";
+
 function MyPage(props) {
   return (
-    <div>
-      <div
-        style={{
-          height: "500px",
-          float: "left",
-        }}
-      >
-        <h2>personal portfolio</h2>
-        <h3> junjoy</h3>
-        <h3> dream : product manager</h3>
-        <h3> stack : python </h3>
-        <br />
-        <address>
-          <a href="https://github.com/mmmjunjoy">
-            <h3> - github -</h3>
-          </a>
-          <a href="https://velog.io/@sjb2010">
-            <h3>- blog -</h3>
-          </a>
-          <br />
-        </address>
-        <section style={{ border: "5px solid gainsboro" }}>
-          <h3> project summary</h3>
-        </section>
-        <section style={{ border: "5px solid gainsboro" }}>
-          <h3> dacon : 모델 성능 높이기 대회 진행중</h3>
-          <h3> icelink : frontend 제작 진행중 </h3>
-        </section>
+    <div class="top-container">
+      <h2>personal portfolio</h2>
+      <div class="myInfo-left">
+        <div class="myInfo-first">
+          <div id="myImage">
+            <img src={myImg}></img>
+          </div>
+          <div id="myName"> junjoy</div>
+          <div class="myOther">
+            <div id="myDream">dream : product manager</div>
+            <div id="myStack">stack : python </div>
+          </div>
+        </div>
+        <hr />
+        <div class="myInfo-link">
+          <div id="mySite">More about</div>
+          <div class="myOther">
+            <div id="myInfo-github">
+              <a href="https://github.com/mmmjunjoy">- github -</a>
+            </div>
+            <div id="myInfo-blog">
+              <a href="https://velog.io/@sjb2010">- blog -</a>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div class="myInfo-projectsum">
+          <div id="projectsum-text">project summary</div>
+          <div class="myOther">
+            <div class="projectsum">
+              <div id="projectsum-title">- dacon</div>
+              <div id="projectsum-info">모델 성능 높이기 대회 진행 중</div>
+            </div>
+            <div class="projectsum">
+              <div id="projectsum-title">- icelink</div>
+              <div id="projectsum-info">frontend 제작 진행 중</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div
-        style={{
-          float: "center",
-          marginLeft: "400px",
-          marginRight: "400px",
-          marginTop: "50px",
-          border: "10px solid gray",
-        }}
-      >
-        <center>
-          <h1> my short introduce</h1>
-        </center>
-      </div>
-      <div
-        style={{
-          float: "center",
-          marginTop: "5px",
-          marginLeft: "400px",
-          marginRight: "400px",
-          border: "5px solid rgb(39, 26, 12)",
-        }}
-      >
-        <center>
-          <h2 style={{ marginLeft: "10px" }}>
+
+      <div class="myInfo-middle">
+        <div class="middle-info myInfo-short">
+          <div class="myInfo-middle-title" id="myInfo-shortIntro">
             {" "}
+            my short introduce
+          </div>
+          <div>
             -- 이 부분이 사용자 입장에서 자유롭게 서술하는 부분--
-          </h2>
-        </center>
-        <h3 style={{ marginLeft: "10px" }}>
-          {" "}
-          안녕하세요 , 저는 한국외국어대학교를 재학중인 심준보입니다.
-        </h3>
-        <h3 style={{ marginLeft: "10px" }}>
-          {" "}
-          저의 꿈은 product manager이며 , 이를 위해 다양한 분야를 경험중입니다.
-        </h3>
-        <h3 style={{ marginLeft: "10px" }}>
-          {" "}
-          현재 , 주는 backend영역을 공부중이며 , 부가적인 요소로 ai 분야와
-          frontend 및 데이터 전처리를 학습하고 있습니다.
-        </h3>
-      </div>
-      <div
-        style={{
-          float: "center",
-          marginTop: "40px",
-          marginLeft: "400px",
-          marginRight: "400px",
-          border: "10px solid gray",
-        }}
-      >
-        <center>
-          <h1> my main introduce</h1>
-        </center>
-      </div>
-      <div
-        style={{
-          float: "center",
-          marginTop: "5px",
-          marginLeft: "400px",
-          marginRight: "400px",
-          border: "5px solid rgb(39, 26, 12)",
-        }}
-      >
-        <center>
-          <h2 style={{ marginLeft: "10px" }}>
+            <br />
+            안녕하세요 , 저는 한국외국어대학교를 재학중인 심준보입니다. 저의
+            꿈은 product manager이며 , 이를 위해 다양한 분야를 경험중입니다.
+            현재 , 주는 backend영역을 공부중이며 , 부가적인 요소로 ai 분야와
+            frontend 및 데이터 전처리를 학습하고 있습니다.
+          </div>
+        </div>
+        <div class="middle-info myInfo-main">
+          <div class="myInfo-middle-title" id="myInfo-mainIntro">
             {" "}
+            my main introduce
+          </div>
+          <div>
             -- 이 부분이 크롤링한 데이터를 이용하여 종합해주는 부분--
-          </h2>
-        </center>
-        <h2 style={{ marginLeft: "10px" }}> stack : python , java , c</h2>
-        <h2 style={{ marginLeft: "10px" }}>
-          {" "}
-          interesting : ai , data processing , frontend , backend
-        </h2>
-        <h2 style={{ marginLeft: "10px" }}>
-          {" "}
-          project : develope icelink platform , greenwashing detect service{" "}
-        </h2>
+            <br />
+            stack : python , java , c interesting : ai , data processing ,
+            frontend , backend project : develope icelink platform ,
+            greenwashing detect service{" "}
+          </div>
+        </div>
       </div>
     </div>
   );
